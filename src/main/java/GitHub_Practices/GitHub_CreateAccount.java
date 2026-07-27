0d22abcd99755b58332d8c9ea262b4b6b7356568
Sai kethan <saikethan022@gmail.com>
2026-07-27 21:28:23 +0530

    Added CreateAccount Link automation

---
+++ src/main/java/GitHub_Practices/GitHub_CreateAccount.java
@@ -0,0 +1,23 @@
+package GitHub_Practices;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GitHub_CreateAccount {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://github.com/login");
+
+        driver.findElement(By.linkText("Create an account")).click();
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+    }
+}
