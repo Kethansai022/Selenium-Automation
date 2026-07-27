dced4b95c3b74844dffc6e6ee287d147fa534ac3
Sai kethan <saikethan022@gmail.com>
2026-07-27 21:11:47 +0530

    Added Username field as your username as automation

---
+++ src/main/java/GitHub_Practices/GitHub_Username.java
@@ -0,0 +1,22 @@
+package GitHub_Practices;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GitHub_Username {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://github.com/login");
+
+        driver.findElement(By.id("login_field"))
+                .sendKeys("your_username");
+
+        driver.quit();
+    }
+}
