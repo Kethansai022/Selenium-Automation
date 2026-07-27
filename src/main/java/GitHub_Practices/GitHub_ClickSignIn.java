f5e2f4202cb419146e3a63d407fe8c8a4bdc179f
Sai kethan <saikethan022@gmail.com>
2026-07-27 21:21:15 +0530

    Added ClickSignIn

---
+++ src/main/java/GitHub_Practices/GitHub_ClickSignIn.java
@@ -0,0 +1,21 @@
+package GitHub_Practices;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GitHub_ClickSignIn {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://github.com/login");
+
+        driver.findElement(By.name("commit")).click();
+
+        driver.quit();
+    }
+}
