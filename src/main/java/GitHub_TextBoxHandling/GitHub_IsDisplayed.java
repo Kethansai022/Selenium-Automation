3823dfbc5c5d49add438918e23020f8b2ced6775
Sai kethan <saikethan022@gmail.com>
2026-07-28 13:22:12 +0530

    Added IsDisplayed Username in TextBoxHandling

---
+++ src/main/java/GitHub_TextBoxHandling/GitHub_IsDisplayed.java
@@ -0,0 +1,28 @@
+package GitHub_TextBoxHandling;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GitHub_IsDisplayed {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://github.com/login");
+
+        WebElement username = driver.findElement(By.id("login_field"));
+
+        System.out.println(username.isDisplayed());
+
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+}
+
