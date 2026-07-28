1d1a78f4b9861174fae8f5d98c1f2b7fd3789ed9
Sai kethan <saikethan022@gmail.com>
2026-07-28 13:29:14 +0530

    Added isEnabled Username in TextBoxHandling

---
+++ src/main/java/GitHub_TextBoxHandling/GitHub_IsEnabled.java
@@ -0,0 +1,26 @@
+package GitHub_TextBoxHandling;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GitHub_IsEnabled {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://github.com/login/");
+
+        WebElement username = driver.findElement(By.id("login_field"));
+
+        System.out.println(username.isEnabled());
+
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+}
