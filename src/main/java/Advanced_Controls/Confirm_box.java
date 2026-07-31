1eab15eb1f45b5b6e33ab87abba913a432bf5c9a
Sai kethan <saikethan022@gmail.com>
2026-07-31 22:03:48 +0530

    Added Alerts (alert box,confirm box,prompt box)

---
+++ src/main/java/Advanced_Controls/Confirm_box.java
@@ -0,0 +1,19 @@
+package Advanced_Controls;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Confirm_box {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.hyrtutorials.com/p/alertsdemo.html#google_vignette");
+        driver.findElement(By.id("confirmBox")).click();
+        Thread.sleep(2000);
+        driver.switchTo().alert().dismiss();
+        Thread.sleep(2000);
+        System.out.println(driver.findElement(By.id("output")).getText());
+        Thread.sleep(2000);
+        driver.quit();
+
+    }
+}
