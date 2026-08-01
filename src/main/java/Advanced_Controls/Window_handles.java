668cb0633a7360aee7b1a1579a84bb7e2fbb91ae
Sai kethan <saikethan022@gmail.com>
2026-08-01 15:29:54 +0530

    Added Windows handling

---
+++ src/main/java/Advanced_Controls/Window_handles.java
@@ -0,0 +1,50 @@
+package Advanced_Controls;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.Set;
+
+public class Window_handles {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Launch Chrome Browser
+        WebDriver driver = new ChromeDriver();
+
+        // Maximize Browser
+        driver.manage().window().maximize();
+
+        // Open Website
+        driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html#");
+        Thread.sleep(1000);
+
+        //store parent window Id & print Id, Title.
+        String parentwindow = driver.getWindowHandle();
+        System.out.println("Parent Window Handle: " + parentwindow + driver.getTitle());
+
+        //opens parent window
+        driver.findElement(By.id("newWindowBtn")).click();
+
+        //get all windows Id's
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        //switch to new window and enter text
+        for (String childwindow : windowHandles) {
+            if (!childwindow.equals(parentwindow)) {
+                driver.switchTo().window(childwindow);
+                driver.manage().window().maximize();
+                Thread.sleep(2000);
+                driver.findElement(By.id("firstName")).sendKeys("sai");
+                Thread.sleep(1000);
+                driver.close();
+            }
+        }
+        //Switch to parent window and enters text
+        driver.switchTo().window(parentwindow);
+        Thread.sleep(2000);
+        driver.findElement(By.id("name")).sendKeys("kethan");
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
